3fb360370010a2060b9e164c16d34f727dad4061
oandrelb <andre.barbosa.1390@gmail.com>
2024-11-06 22:03:57 -0300

    testando os dados retornados da API utilizada no projeto

--- src/js/controller.js
+++ src/js/controller.js
@@ -10,4 +10,34 @@ const timeout = function (s) {
 
 // https://forkify-api.herokuapp.com/v2
 
+// https://forkify-api.herokuapp.com/api/v2/recipes?search=${nomeDoPrato}&key=c5a05781-2a28-4dcc-bb57-630183e7181d
+
 ///////////////////////////////////////
+
+const buscarReceita = async function (nomeDoPrato) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza&key=83a663a4-838c-4dfa-b018-d7b9a3945d12`
+    );
+
+    const data = await res.json();
+
+    if (data.status === 'fail')
+      throw new Error(`${data.message} (${res.status})`);
+
+    console.log(res, data);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      image: recipe.image,
+    };
+    console.log(recipe);
+  } catch (error) {
+    console.error(error);
+    alert(error);
+  }
+};
+buscarReceita();
